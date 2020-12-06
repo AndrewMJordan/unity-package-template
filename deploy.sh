d85e686c2b5ed270162a8276d7b58d253c513c99
Andrew Jordan <andrew.m.jordan@hotmail.com>
2020-12-06 13:29:47 -0800

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Deploy UPM branch
+# Author:	AndrewMJordan
+# Version:	1.0.0
+
+# Validate parameters
+if [ -z "$1" ]
+then
+	echo "Parameter \"packageRoot\" is invalid"
+	echo "Usage: ./deploy.sh <packageRoot>"
+	exit 1
+fi
+
+packageRoot="$1"
+
+# Prepare package assets
+find "$packageRoot" \( -iname "*.cs" -o -iname "*.shader" \) | while read fname; do
+	cat "preamble.txt" | goo transform inject "${fname}"
+done
+goo transform hide-folder "${packageRoot}/Samples"
+goo package ignore-folder "${packageRoot}" "Samples"
+
+# Preserve package files
+tmp_dir=$(mktemp -d -t deploy-XXXX)
+cp -r "$packageRoot"/. "$tmp_dir"/
+
+# Prepare working tree for upm branch
+git checkout -f upm 2> /dev/null || (git checkout -fb upm)
+git clean -fd && git rm -r *
+# Restore package files
+mv "$tmp_dir"/* .
+rm -rf $tmp_dir
+
+# Commit
+version=`goo package get-version .`
+git add -A
+git commit -m "Deploy $version"
+git tag -a -m "Release tag" "$version"
+
+# Push
+git push origin upm && git push origin --tags
