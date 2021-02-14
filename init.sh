fe637cfb9df91e41495e1a5300d11e58e3b55c0d
Andrew Jordan <andrewmichaeljordan@outlook.com>
2021-02-14 15:19:12 -0800

    Use proper git

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ read -p 'Author: ' author
 
 sed -i "s/<AUTHOR>/$author/g" LICENSE
 sed -i "s/<AUTHOR>/$author/g" preamble.txt
-mv "Project" "$packageName"
+git mv "Project" "$packageName"
 sed -i "s/productName: Test Package/productName: $packageName/g" "$packageName/ProjectSettings/ProjectSettings.asset"
-mv "$packageName/Assets/Package" "$packageName/Assets/$packageName"
-mv "$packageName/Assets/Package.meta" "$packageName/Assets/$packageName.meta"
+git mv "$packageName/Assets/Package" "$packageName/Assets/$packageName"
+git mv "$packageName/Assets/Package.meta" "$packageName/Assets/$packageName.meta"
