069e4a47c90c6a618fa7b2b34f99fc7b082723e7
Andrew Jordan <andrewmichaeljordan@outlook.com>
2021-02-14 15:16:18 -0800

    Fix wizard

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ read -p 'Author: ' author
 sed -i "s/<AUTHOR>/$author/g" LICENSE
 sed -i "s/<AUTHOR>/$author/g" preamble.txt
 mv "Project" "$packageName"
-sed -i "s/productName: Test Package/productName: $packageName/g"
+sed -i "s/productName: Test Package/productName: $packageName/g" "$packageName/ProjectSettings/ProjectSettings.asset"
 mv "$packageName/Assets/Package" "$packageName/Assets/$packageName"
-mv "$packageName/Assets/Package.md" "$packageName/Assets/$packageName.md"
+mv "$packageName/Assets/Package.meta" "$packageName/Assets/$packageName.meta"
