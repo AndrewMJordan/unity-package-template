ced894fbb04ee6e3fe10626bdec3e1cd8799a8a3
Andrew Jordan <andrewmichaeljordan@outlook.com>
2021-02-15 00:17:26 -0800

    Update configuration wizard

--- init.sh
+++ init.sh
@@ -3,9 +3,14 @@
 read -p 'Package Name: ' packageName
 read -p 'Author: ' author
 
+projectName=$packageName
+
 sed -i "s/<AUTHOR>/$author/g" LICENSE
 sed -i "s/<AUTHOR>/$author/g" preamble.txt
-git mv "Project" "$packageName"
-sed -i "s/productName: Test Package/productName: $packageName/g" "$packageName/ProjectSettings/ProjectSettings.asset"
-git mv "$packageName/Assets/Package" "$packageName/Assets/$packageName"
-git mv "$packageName/Assets/Package.meta" "$packageName/Assets/$packageName.meta"
+git mv "Project" "$projectName"
+sed -i "s/productName: Project/productName: $projectName/g" "$projectName/ProjectSettings/ProjectSettings.asset"
+git mv "$projectName/Assets/Package" "$projectName/Assets/$packageName"
+git mv "$projectName/Assets/Package.meta" "$projectName/Assets/$packageName.meta"
+
+sed -i "s/<PROJECT_NAME>/$projectName/g" ".github/workflows/main.yml"
+sed -i "s/<PACKAGE_NAME>/$packageName/g" ".github/workflows/main.yml"
